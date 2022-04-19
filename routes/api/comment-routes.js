9601cf3621d0d3323f8bdcb0120f2f444341c8ed
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-19 14:21:10 -0700

    create DELETE route for comments

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -25,8 +25,24 @@ router.post('/', (req, res) => {
     });
 });
 
+// delete a comment
 router.delete('/:id', (req, res) => {
-
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbCommentData => {
+        if(!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with this ID!' });
+            return;
+        }
+        res.json(dbCommentData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;

352530af9f4fd97dd8bb71f18bfea0d2bb7ef6e4
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-19 14:14:53 -0700

    create POST route for commenting

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -5,12 +5,22 @@ router.get('/', (req, res) => {
 
 });
 
+// create a comment
 router.post('/', (req, res) => {
-
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
 
 });
 
-module.exports = Router;
+module.exports = router;

0c35152fa02c2dbf0ba18f1e9922c07abc0a6652
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-19 14:17:17 -0700

    create GET route for comments

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
 
+// get all comments
 router.get('/', (req, res) => {
-
+    Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 // create a comment

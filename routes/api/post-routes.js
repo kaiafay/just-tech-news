2951d25dbe0262a3fded8bae46471504c4082931
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-19 14:35:11 -0700

    edit post routes to include comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 // get all posts
 router.get('/', (req, res) => {
-    console.log('===================');
+    console.log('======================');
     Post.findAll({
         attributes: ['id', 'post_url', 'title', 'created_at',
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
@@ -13,6 +13,14 @@ router.get('/', (req, res) => {
         order: [['created_at', 'DESC']],
         // include the JOIN to the User table
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -36,6 +44,14 @@ router.get('/:id', (req, res) => {
             [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['Username']

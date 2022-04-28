2048b15474736cfdf8da5288e2f8c72a26b73832
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-28 09:52:06 -0700

    conditionally render the login links

--- controllers/home.routes.js
+++ controllers/home.routes.js
@@ -35,7 +35,7 @@ router.get('/', (req, res) => {
           // sequelize's get() method allows us to serialize the object down to only the properties we need   
           const posts = dbPostData.map(post => post.get({ plain: true}));
           // pass a single post object into the homepage template
-          res.render('homepage', { posts });
+          res.render('homepage', { posts, loggedIn: req.session.loggedIn });
         })
         .catch(err => {
           console.log(err);

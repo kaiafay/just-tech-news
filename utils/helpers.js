75832670824144ab4b138d238ff325e7b1a75fdb
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-28 13:35:51 -0700

    write test and helper function for URL formatting

--- utils/helpers.js
+++ utils/helpers.js
@@ -12,5 +12,15 @@ module.exports = {
             return `${word}s`;
         }
         return word;
-    }
+    },
+
+    // helper function for URL format
+    format_url: url => {
+        return url
+          .replace('http://', '')
+          .replace('https://', '')
+          .replace('www.', '')
+          .split('/')[0]
+          .split('?')[0];
+      }
 };

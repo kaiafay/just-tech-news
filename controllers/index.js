a73f5583bdddca433bfbad12daa90173c1a1e101
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-29 15:51:08 -0700

    create route to dashboard page

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 // import packaged api routes
 const apiRoutes = require('./api');
 const homeRoutes = require('./home.routes');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 // prefixing the packaged group of api endpoints with the path /api
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 // recieve 404 error if making a request to an endpoint that doesn't exist
 router.use((req, res) => {
